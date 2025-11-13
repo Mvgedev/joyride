db062dc9f1533cd90efb9a9846bdf16275758a54
Mvge <mvge.dev@gmail.com>
2025-11-13 15:32:43 +0100

    Initial commit
    - Godot project created
    - Added PlayerCharacter node
    - Added jetpack base system
    - Added ground, ceiling, and spawn/despawn borders to gameScene
    - Added temporary sprite for player

---
+++ scripts/player_character.gd
@@ -0,0 +1,51 @@
+extends RigidBody2D
+
+#jetpack values
+var max_velocity = -300
+var max_downfall = 400
+
+enum EQUIPMENT {JETPACK, CLOUD, MOTO}
+
+var cur_equip = EQUIPMENT.JETPACK
+
+var on_ground = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(_delta: float) -> void:
+	match cur_equip:
+		EQUIPMENT.JETPACK:
+			process_jetpack()
+		EQUIPMENT.CLOUD:
+			pass
+		EQUIPMENT.MOTO:
+			pass
+	
+
+func process_jetpack():
+	var strength = 0
+	if Input.is_action_just_pressed("ui_accept"):
+		if linear_velocity.y > 70:
+			linear_velocity.y = linear_velocity.y / 2 # Cut down linear velocity for downfall
+	elif Input.is_action_just_released("ui_accept"):
+		if linear_velocity.y < -70:
+			linear_velocity.y = linear_velocity.y / 2
+	if Input.is_action_pressed("ui_accept"):
+		if linear_velocity.y > max_velocity:
+			strength = -650
+	apply_force(Vector2(0, strength))
+	if linear_velocity.y < max_velocity:
+		linear_velocity.y = max_velocity
+	elif linear_velocity.y > max_downfall:
+		linear_velocity.y = max_downfall
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("yes")
+	on_ground = true
+	pass # Replace with function body.
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	on_ground = false
+	pass # Replace with function body.
